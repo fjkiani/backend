758cc4d984cd5d4b4883fc3ed8584542a4b68549
Fahad Kiani <fjkiani@icloud.com>
2025-01-27 16:55:01 -0500

    fixed build issues

--- src/types/logger.ts
+++ src/types/logger.ts
@@ -1,6 +1,6 @@
 export interface ILogger {
-  info(message: string, ...args: any[]): void;
-  error(message: string, ...args: any[]): void;
-  warn(message: string, ...args: any[]): void;
-  debug(message: string, ...args: any[]): void;
+  info(message: string, meta?: any): void;
+  error(message: string, meta?: any): void;
+  warn(message: string, meta?: any): void;
+  debug(message: string, meta?: any): void;
 } 
